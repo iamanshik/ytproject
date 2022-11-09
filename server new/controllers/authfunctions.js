ec475fe34814cfd3c8d54915be486ee1689c60ef
anshik <anshik@kali.lab.local>
2022-11-09 11:11:27 +0530

    jwt verification pending

--- server new/controllers/authfunctions.js
+++ server new/controllers/authfunctions.js
@@ -1,15 +1,19 @@
-const login = (req,res)=>{
-
-}
-const signup = async(req,res)=>{
-     const response = await creds.create(
-      {  name: "anshik",
-        email: 'hi there i am a description',
-        password: "i am an strong password"}
-    )
-    console.log(response)
-    res.send("succesfull")
-}
+const { creds } = require("../models/Schema");
+const login = (req, res) => {
+    res.send("pending kid")
+};
+const signup = async (req, res) => {
+    
+  const response = await creds.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  console.log(response);
+//   console.log(req.body);
+  res.send("succesfull");
+};
 module.exports = {
-    signup,login
-}
+  signup,
+  login,
+};
--- server new/routes/authrouters.js
+++ server new/routes/authrouters.js
@@ -1,19 +1,6 @@
 const express = require('express')
-const { creds} =require('../models/Schema')
+const {login,signup} = require('../controllers/authfunctions')
 const authrouters = express.Router()
-authrouters.post("/login",(req,res)=>{
-    res.send("login successfull")
-})
-authrouters.post("/signup",(req,res)=>{
-    const response = creds.create(
-      {  name: "anshik",
-        email: 'hi there i am a description',
-        password: "i am an strong password"}
-    )
-    console.log(response)
-    res.send("succesfull")
-})
-authrouters.post("/jwtverify",(req,res)=>{
-    res.send("login successfull")
-})
+authrouters.post("/login",login)
+authrouters.route("/signup").post(signup)
 module.exports = authrouters
